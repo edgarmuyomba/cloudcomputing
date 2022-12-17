dfc7fad44d7778cf7398cabe9bb989e0093e586d
edgarmatthew <edgarmatthew247@gmail.com>
2022-12-17 16:13:49 +0300

    new changes

--- tweetwave/settings.py
+++ tweetwave/settings.py
@@ -124,10 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'styles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles_cdn')
